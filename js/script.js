dfef49f3690889e9f2cf72bfb57830b23648349e
Shaxzod777 <106257760+Shaxzod777@users.noreply.github.com>
2025-06-18 18:51:17 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const tabheader = document.querySelectorAll(".tabheader__item")
+const tabcontent = document.querySelectorAll(".tabcontent")
+
+
+
+let images = {
+    fitnes: "./img/tabs/vegy.jpg",
+    premium: "./img/tabs/elite.jpg",
+    post: "./img/tabs/post.jpg"
+}
+
+
+tabheader.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        tabheader.forEach(el => {
+            el.classList.remove("active")
+            
+        })
+        tabcontent.forEach(b => {
+             b.classList.add("hide")
+        })
+
+        tabcontent[i].classList.remove("hide")
+        item.classList.add("active")
+       
+    })
+})
+
+
